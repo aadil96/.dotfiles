5baeeec709df6cc7adad1699e2b6b75ff5b02b7b
aadil96 <agwan96@gmail.com>
2021-08-19 20:53:38 +0530

    add: script to install zsh set as default shell

---
+++ fresh.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+
+echo "Setting up your linux machine..."
+
+sudo apt update
+
+sudo apt install zsh
+
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+chsh -s $(which zsh)
+
+source ~/.zshrc
