0e2a7cad3c6037e857f36a49e8c323f74c8f2e8b
aadil96 <agwan96@gmail.com>
2021-08-22 19:54:51 +0530

    add: alias to halt vagrant

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,19 @@
+# dir
+alias dls="cd ~/Downloads"
 alias hstead="cd ~/Development/Homestead"
+alias dev="cd ~/Development"
+
+# vagrant
 alias vussh="cd ~/Development/Homestead && vagrant up && vagrant ssh"
 alias vup="vagrant up"
 alias vssh="vagrant ssh"
-alias dev="cd ~/Development"
+alias vexit="vagrant halt && exit"
+alias vhalt="vagrant halt"
+
+# git
 alias gpull="git pull $1 $2"
 alias gpush="git push $1 $2"
 alias gseturl="git remote set-url origin https://$1:$2@github.com/$3/$4"
 alias gaddurl="git remote add-url origin https://$1:$2@github.com/$3/$4"
+alias guser="git config user.name $1"
+alias gemail="git config user.email $1"
